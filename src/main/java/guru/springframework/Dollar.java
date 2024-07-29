7adcfde201fb72ef72b5cd5443c65a88a796a9b3
Brian Vega <BrianVega01@outlook.es>
2024-07-29 11:04:05 -0600

    21. Adding Francs

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -1,7 +1,7 @@
 package guru.springframework;
 
 public class Dollar {
-    int amount;
+    private int amount;
 
     public Dollar(int amount) {
         this.amount = amount;
@@ -9,5 +9,11 @@ public class Dollar {
 
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals (Object object) {
+        Dollar dollar = (Dollar) object;
+        return dollar.amount == this.amount;
     }
 }
