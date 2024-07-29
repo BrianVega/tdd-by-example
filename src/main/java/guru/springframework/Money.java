edfc7a9ef6fb5249d5c3d5c7afa31d9b3e82f396
Brian Vega <BrianVega01@outlook.es>
2024-07-29 12:18:44 -0600

    26. Interesting times, refactoring common times

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -1,6 +1,8 @@
 package guru.springframework;
 
-public abstract class Money {
+import java.util.Objects;
+
+public class Money {
     protected int amount;
     protected String currency;
 
@@ -14,7 +16,9 @@ public abstract class Money {
         return currency;
     }
 
-    public abstract Money times(int multiplier);
+    public Money times(int multiplier){
+        return new Money(amount * multiplier, currency);
+    }
 
     public static Money dollar(int amount) {
         return new Dollar(amount, "USD");
@@ -27,6 +31,14 @@ public abstract class Money {
     public boolean equals (Object object) {
         Money money = (Money) object;
         return amount == money.amount
-                && this.getClass().equals(object.getClass());
+                && Objects.equals(this.currency, money.currency);
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
     }
 }
